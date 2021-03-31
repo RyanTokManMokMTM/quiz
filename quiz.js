b3b3c10832dbb74c7fad4d2819495f65116bcc2c
JackSon_tm.m <RyanTokManMokMTM@hotmail.com>
2021-03-31 10:16:05 +0800

    updated

--- quiz.js
+++ quiz.js
@@ -2,6 +2,6 @@ var change = () => {
     var header = document.getElementById("cgu")
     var para = document.getElementById("para")
 
-    header[0].innerText = "CGU@CGU.edu.tw"
-    para[0].innerText = "Very Nice"
+    header.innerText = "CGU@CGU.edu.tw"
+    para.innerText = "Very Nice"
 }
