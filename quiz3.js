8124cd666b0485e919336cc246b29eeb744c2c5f
JackSon_tm.m <RyanTokManMokMTM@hotmail.com>
2021-04-21 11:08:31 +0800

    Delete quiz3.js

--- quiz3.js
+++
@@ -1,8 +0,0 @@
-var typing = () => {
-    var p = document.querySelector("#text")
-
-    document.addEventListener("keydown", (e) => {
-        console.log(e.key)
-        p.textContent += e.key
-    });
-}
