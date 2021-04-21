699e0a6dc04d961f141ce33bdb5eef95a87ee117
JackSon_tm.m <RyanTokManMokMTM@hotmail.com>
2021-04-21 11:08:05 +0800

    Delete quiz.js

--- quiz.js
+++
@@ -1,7 +0,0 @@
-var change = () => {
-    var header = document.getElementById("cgu")
-    var para = document.getElementById("para")
-
-    header.innerText = "CGU@CGU.edu.tw"
-    para.innerText = "Very Nice"
-}
